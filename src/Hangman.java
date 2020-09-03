24d7d3169da22d5509da22ac17a7227a6b47d3c6
Jakob Penninger <jakob.penninger@gmx.net>
2020-09-03 13:32:32 +0200

    Add Hangman completed

--- src/Hangman.java
+++ src/Hangman.java
@@ -19,7 +19,6 @@ public class Hangman {
 
         for (int i = 0; i < secret.size(); i++) {
             System.out.print(secret.get(i));
-
         }
 
         System.out.println();
@@ -28,25 +27,153 @@ public class Hangman {
             letters.add(secretWord.charAt(i));
         }
 
-        while (!secret.equals(letters)) {
+        int tries = 0;
+        boolean foundChar = true;
 
-            System.out.print("Buchstabe: ");
+        while (!secret.equals(letters) && tries < 13) {
+
+            System.out.println("\nBuchstabe: ");
             char input = sc.nextLine().charAt(0);
+
+            int foundRight = 0;
+
             for (int i = 0; i < letters.size(); i++) {
 
                 char c = letters.get(i);
+
                 if (input == c) {
                     secret.set(i, input);
+                    foundRight++;
                 }
+
+            }
+
+            if (foundRight == 0) {
+                foundChar = false;
             }
 
             for (char c : secret) {
                 System.out.print(c);
             }
 
-            System.out.println();
+            if (!foundChar) {
+
+                tries++;
+
+                switch (tries) {
+                    case 1:
+                        System.out.println("\n___|___");
+                        break;
+                    case 2:
+                        System.out.println("\n   |");
+                        System.out.println("___|___");
+                        break;
+                    case 3:
+                        System.out.println("\n   |");
+                        System.out.println("   |");
+                        System.out.println("___|___");
+                        break;
+                    case 4:
+                        System.out.println("\n   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("___|___");
+                        break;
+                    case 5:
+                        System.out.println("\n   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("___|___");
+                        break;
+                    case 6:
+                        System.out.println("\n   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("___|___");
+                        break;
+                    case 7:
+                        System.out.println("\n   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("___|___");
+                        break;
+                    case 8:
+                        System.out.println("\n       _______");
+                        System.out.println("   |/");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("___|___");
+                        break;
+                    case 9:
+                        System.out.println("\n       _______");
+                        System.out.println("   |/      |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("___|___");
+                        break;
+                    case 10:
+                        System.out.println("\n       _______");
+                        System.out.println("   |/      |");
+                        System.out.println("   |      (_)");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("___|___");
+                        break;
+                    case 11:
+                        System.out.println("\n       _______");
+                        System.out.println("   |/      |");
+                        System.out.println("   |      (_)");
+                        System.out.println("   |      \\|/");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("___|___");
+                        break;
+                    case 12:
+                        System.out.println("\n       _______");
+                        System.out.println("   |/      |");
+                        System.out.println("   |      (_)");
+                        System.out.println("   |      \\|/");
+                        System.out.println("   |       |");
+                        System.out.println("   |");
+                        System.out.println("   |");
+                        System.out.println("___|___");
+                        break;
+                    case 13:
+                        System.out.println("\n       _______");
+                        System.out.println("   |/      |");
+                        System.out.println("   |      (_)");
+                        System.out.println("   |      \\|/");
+                        System.out.println("   |       |");
+                        System.out.println("   |      / \\");
+                        System.out.println("   |");
+                        System.out.println("___|___");
+                        break;
+                    default:
+                        System.out.println("\nDu bist gescheitert!");
+
+                }
+            }
+
+            foundChar = true;
 
         }
+
+        System.out.println();
 
         sc.close();
 
@@ -57,6 +184,8 @@ public class Hangman {
         }
 
         System.out.println("\nSpiel beendet");
+
+
     }
 
 }
